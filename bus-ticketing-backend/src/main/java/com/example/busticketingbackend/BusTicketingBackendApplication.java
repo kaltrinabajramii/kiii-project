8792e64352f91ac8544d3b7423344f7389d9a8c0
kaltrinabajramii <kaltro.bajrami@icloud.com>
2026-02-07 00:09:38 +0100

    Initial project setup: Spring Boot backend + React frontend

---
+++ bus-ticketing-backend/src/main/java/com/example/busticketingbackend/BusTicketingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.busticketingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusTicketingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run ( BusTicketingBackendApplication.class, args );
+    }
+
+}
---
+++ bus-ticketing-backend/src/test/java/com/example/busticketingbackend/BusTicketingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.busticketingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusTicketingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
